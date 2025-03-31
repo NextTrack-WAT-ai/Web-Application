2583b07b97a721eb907c3ca749a180bc9c35305e
Umar <umardxbfarooq@gmail.com>
2025-03-31 00:06:37 -0400

    Added backend route to obtain all tracks from user playlist by ID

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,6 +1,9 @@
 import Stepper from "./Stepper";
+import {useState} from 'react'; 
 
 export default function Landing() {
+    const [playlistId, setPlaylistId] = useState(""); 
+
     const getUserArtists = async () => {
         const response = await fetch("http://localhost:8080/api/user-top-artists"); 
         const text = await response.text(); 
@@ -8,15 +11,30 @@ export default function Landing() {
     }
 
     const getUserPlaylists = async () => {
-        const response = await fetch("http://localhost:8080/api/all-user-playlists"); 
+        const response = await fetch("http://localhost:8080/api/playlists/all"); 
         const text = await response.text(); 
         console.log(JSON.parse(text)); // gives us an array of playlists
+    }
+
+    const getPlaylist = async (playListId:string) => {
+        const response = await fetch(`http://localhost:8080/api/playlist?id=${playListId}`, {
+            method : "GET", 
+        });0
+        const text = await response.text(); 
+        console.log(JSON.parse(text));  
+    }
+
+    const handleIdChange = (e:any) => {
+        e.preventDefault(); 
+        setPlaylistId(e.target.value); 
     }
 
     return (
         <div>
             <button onClick={getUserArtists}>Get top user artists</button>
             <button onClick={getUserPlaylists}>Get top user playlists</button>
+            <input type="text" value = {playlistId} onChange={handleIdChange}></input>
+            <button onClick={() => getPlaylist(playlistId)}>Get Playlist</button>
             <Stepper />
         </div>
     )
--- spring-boot-app/src/main/java/com/nexttrack/spring_boot_app/controller/PlaylistController.java
+++ spring-boot-app/src/main/java/com/nexttrack/spring_boot_app/controller/PlaylistController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.nexttrack.spring_boot_app.Services.SpotifyService;
 
-import se.michaelthelin.spotify.model_objects.specification.Artist;
 import se.michaelthelin.spotify.model_objects.specification.PlaylistSimplified;
-import se.michaelthelin.spotify.model_objects.specification.TrackSimplified;
+import se.michaelthelin.spotify.model_objects.specification.PlaylistTrack;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequestMapping("/api")
@@ -17,9 +16,9 @@ public class PlaylistController {
 
     private SpotifyService spotifyService;
     
-        public PlaylistController(SpotifyService spotifyService){
-            this.spotifyService = spotifyService;
-        }
+    public PlaylistController(SpotifyService spotifyService){
+        this.spotifyService = spotifyService;
+    }
             
     @GetMapping("playlists/all")
     public PlaylistSimplified[] getAllPlaylists() {
@@ -27,9 +26,7 @@ public class PlaylistController {
     }
     
     @GetMapping("playlist")
-    public TrackSimplified[] getPlaylist(@RequestParam("id") String playlistId) {        
-        return spotifyService.getSongsFromPlaylist();
+    public PlaylistTrack[] getPlaylist(@RequestParam("id") String playlistId) {        
+        return spotifyService.getSongsFromPlaylist(playlistId);
     }
-
-    
 }
